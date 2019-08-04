0d675dca19bfe97ffaf548c00b5589f88cec577c
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata176738@correo.itm.edu.co>
2019-08-03 20:48:15 -0500

    Database controller owner and seed db

---
+++ Leases/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Leases.Data.Entities;
+
+namespace Leases.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        public DbSet<Lessee> Lessees { get; set; }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+    }
+
+}
---
+++ Leases/Data/Entities/PropertyImage.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Leases.Data.Entities
+{
+    public class PropertyImage
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string ImageUrl { get; set; }
+
+        public Property Property { get; set; }
+
+        // TODO: Change the path when publish
+        public string ImageFullPath => $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+
+        public PropertyType PropertyType { get; set; }
+
+    }
+
+}
